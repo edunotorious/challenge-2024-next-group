7006f76130c6c20b796b3d238ed6cdb8326548e8
Gustavo <gus@smartpay.com.vc>
2024-10-10 22:17:05 -0300

    front

---
+++ front/virtual-scopia/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+	return (
+		<>
+			<div className={"flex justify-center"}>
+				hello world
+				<ul>
+					<li>
+						<div>dada</div>
+					</li>
+					<li>
+						<div>dada</div>
+					</li>
+					<li>
+						<div>dada</div>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
+}
+
+export default App;

09e52afd7b1cced6234a0571748772bb13cfb5a7
Gustavo <gus@smartpay.com.vc>
2024-10-17 21:59:06 -0300

    branch devel

--- front/virtual-scopia/src/main.jsx
+++ front/virtual-scopia/src/main.jsx
@@ -1,12 +1,11 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css'; 
-import AppRoutes from './routes/AppRoutes';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import AppRoutes from "./routes/AppRoutes";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <AppRoutes />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<AppRoutes />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
--- front/virtual-scopia/src/pages/Home/Home.jsx
+++ front/virtual-scopia/src/pages/Home/Home.jsx
@@ -1,44 +1,38 @@
-import React, { useState } from 'react';
-import './index.css';
+import React, { useState } from "react";
+import "./index.css";
 
 const Home = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
+	const handleLogin = (e) => {
+		e.preventDefault();
+		console.log("Username:", username);
+		console.log("Password:", password);
+	};
 
-  return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div className="form-group">
-          <label htmlFor="username">HOME:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="login-container">
+			<h2>Login</h2>
+			<form onSubmit={handleLogin}>
+				<div className="form-group">
+					<label htmlFor="username">HOME:</label>
+					<input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						id="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</div>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
 };
 
 export default Home;
